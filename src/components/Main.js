bc11fd1e2f9e601ba277f331725b0a883a4c4af8
DK <dkdan94@gmail.com>
2018-10-24 22:53:07 -0400

    render react component

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Countries extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div>hello</div>;
+  }
+}
+
+export default Countries;
